1ab5a0b6e45be0829482233d6f3c3858233b96b9
Nico Guerin <guerinnico08@gmail.com>
2023-11-20 09:58:41 +0100

    afegir spinner a la pàgina d'hotels i mostrar una llista temporal d'hotels

--- app/src/main/java/com/example/projectgitm8/Hotels.java
+++ app/src/main/java/com/example/projectgitm8/Hotels.java
@@ -3,12 +3,45 @@ package com.example.projectgitm8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
-public class Hotels extends AppCompatActivity {
-
+public class Hotels extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotels);
+
+        Spinner spinner = findViewById(R.id.categories);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.hotelCategories,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+                this,
+                R.array.hotelList,
+                android.R.layout.simple_list_item_1
+        );
+
+        ListView listView = findViewById(R.id.hotelList);
+        listView.setAdapter(adapter2);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
