ad3256dfad88fef2afec874795f9afbaff9f8016
Shavie5 <xaviikerarechavala@gmail.com>
2023-11-20 12:26:18 +0100

    Sus toolbar (toolbar rarete)

--- app/src/main/java/com/example/projectgitm8/Restaurants.java
+++ app/src/main/java/com/example/projectgitm8/Restaurants.java
@@ -3,12 +3,35 @@ package com.example.projectgitm8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class Restaurants extends AppCompatActivity {
+public class Restaurants extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        Spinner spinner = findViewById(R.id.rSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.restaurantCategories,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
