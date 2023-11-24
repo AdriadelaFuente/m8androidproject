b981a0b6918cdf302fa764c5d6a6be62ad2b0558
Adrià de la Fuente <adria.fuente.bitbucket@tusmedia.com>
2023-11-24 19:58:59 +0100

    Pujada

--- app/src/main/java/com/example/projectgitm8/Businesses.java
+++ app/src/main/java/com/example/projectgitm8/Businesses.java
@@ -3,12 +3,28 @@ package com.example.projectgitm8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Businesses extends AppCompatActivity {
 
+    TextView tv1, tv2, tv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_businesses);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, R.array.spinner_array);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
+
 }
